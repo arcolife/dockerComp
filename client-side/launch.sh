3386dde86d4126ef214dc34069d51134087f9d1e
Archit Sharma <arcshams@gmail.com>
2014-11-11 05:27:24 -0500

    fix no. of containers for which detail is sent to server

--- client-side/launch.sh
+++ client-side/launch.sh
@@ -12,7 +12,7 @@ docker run -p :80 -d arcolife/docker_comp
 # cd scripts/
 # python read_container_details.py
 # cd ..
-docker inspect $(docker ps -q) > container_info.json
+docker inspect $(docker ps -q | head -n -4) > container_info.json
 curl -H "Content-type: application/json" -X POST http://104.131.170.133/get_details/ -d @container_info.json
 
 echo "4 containers have been deployed.."
