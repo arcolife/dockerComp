efdea882b26dc21b97306592266f9958335c0fb9
Archit Sharma <arcsharm@redhat.com>
2014-11-11 15:37:48 +0530

    refactor container launch process

--- client-side/launch.sh
+++ client-side/launch.sh
@@ -8,10 +8,11 @@ docker run -p :80 -d arcolife/docker_comp
 docker run -p :80 -d arcolife/docker_comp
 docker run -p :80 -d arcolife/docker_comp
 
-# echo "4 containers are ready to be deployed for analysis"
-
-docker ps > containers_details
-cd scripts/
-python read_container_details.py
-cd ..
+# docker ps > containers_details
+# cd scripts/
+# python read_container_details.py
+# cd ..
+docker inspect $(docker ps -q) > container_info.json
 curl -H "Content-type: application/json" -X POST http://104.131.170.133/get_details/ -d @container_info.json
+
+echo "4 containers have been deployed.."
