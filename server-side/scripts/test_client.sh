ed46a7bb31bba428f2126883ea52c45508a3ea7d
Archit Sharma <arcshams@gmail.com>
2014-11-11 02:38:53 -0500

    correct connection parameters for client test script

--- server-side/scripts/test_client.sh
+++ server-side/scripts/test_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "Content-type: application/json" -X POST http://0.0.0.0:5000/tasks/ -d '[(1,2),(5,2),(3,5)]'
+curl -H "Content-type: application/json" -X POST http://104.131.170.136:49153/tasks/ -d '[(1,2),(5,2),(3,5)]'
